f36f202f3bed2bc26324167db4160a06ed68d2d7
LLeonteva <leontievall@gmail.com>
2019-04-25 20:50:14 -0700

    Created By.CSS_SELECTOR instead of By.XPATH in SignIn page

--- features/steps/amazon_page_signin_steps.py
+++ features/steps/amazon_page_signin_steps.py
@@ -4,22 +4,43 @@ from time import sleep
 
 
 # =============================== LOCATORS ================================================
-AMAZON_LOGO = (By.XPATH, "//i[@class='a-icon a-icon-logo']")
+# AMAZON_LOGO = (By.XPATH, "//i[@class='a-icon a-icon-logo']")
 # AMAZON_LOGO = (By.XPATH, "//a[@href='/ref=ap_frn_logo']")
-SIGN_IN_LOGO = (By.XPATH, "//h1[@class='a-spacing-small']")
-EMAIL_FIELD = (By.ID, 'ap_email')
+AMAZON_LOGO = (By.CSS_SELECTOR, "i.a-icon.a-icon-logo")
+
+# SIGN_IN_LOGO = (By.XPATH, "//h1[@class='a-spacing-small']")
+SIGN_IN_LOGO = (By.CSS_SELECTOR, "h1.a-spacing-small")
+
+# EMAIL_FIELD = (By.ID, 'ap_email')
+EMAIL_FIELD = (By.CSS_SELECTOR, "input#ap_email")
 # EMAIL_FIELD = (By.XPATH, "//input[@id='ap_email']")
-PASSWORD_FIELD = (By.ID, 'ap_password')
+
+# PASSWORD_FIELD = (By.ID, 'ap_password')
+PASSWORD_FIELD = (By.CSS_SELECTOR, "input#ap_password")
 # PASSWORD_FIELD = (By.XPATH, "//input[@id='ap_password']")
-FORGOT_PASSWORD_LINK = (By.ID, 'auth-fpp-link-bottom')
-SIGN_IN_BTN = (By.ID, 'signInSubmit')
-KEEP_SIGNED_CHECKBOX = (By.NAME, 'rememberMe')
+
+# FORGOT_PASSWORD_LINK = (By.ID, 'auth-fpp-link-bottom')
+FORGOT_PASSWORD_LINK = (By.CSS_SELECTOR, "a#auth-fpp-link-bottom")
+
+# SIGN_IN_BTN = (By.ID, 'signInSubmit')
+SIGN_IN_BTN = (By.CSS_SELECTOR, "input#signInSubmit")
+
+# KEEP_SIGNED_CHECKBOX = (By.NAME, 'rememberMe')
+KEEP_SIGNED_CHECKBOX = (By.CSS_SELECTOR, "div.a-checkbox")
 # KEEP_SIGNED_CHECKBOX = (By.XPATH, "//input[@type='checkbox']")
-DETAILS_LINK = (By.ID, "remember_me_learn_more_link")
-CREAT_ACCOUNT_LINK = (By.ID, "createAccountSubmit")
-COU_LINK = (By.XPATH, "//a[contains(@href,'ap_desktop_footer_cou')]")
+
+# DETAILS_LINK = (By.ID, "remember_me_learn_more_link")
+DETAILS_LINK = (By.CSS_SELECTOR, "a#remember_me_learn_more_link")
+
+# CREAT_ACCOUNT_LINK = (By.ID, "createAccountSubmit")
+CREAT_ACCOUNT_LINK = (By.CSS_SELECTOR, "a#createAccountSubmit")
+
+# COU_LINK = (By.XPATH, "//a[contains(@href,'ap_desktop_footer_cou')]")
+COU_LINK = (By.CSS_SELECTOR, "[href*='ap_desktop_footer_cou']")
 # COU_LINK = (By.XPATH, "//a[contains(@href, 'ap_signin_notification_condition_of_use')")
-PRIVACY_NOTICE_LINK = (By.XPATH, "//a[contains(@href,'ap_desktop_footer_privacy_notice')]")
+
+# PRIVACY_NOTICE_LINK = (By.XPATH, "//a[contains(@href,'ap_desktop_footer_privacy_notice')]")
+PRIVACY_NOTICE_LINK = (By.CSS_SELECTOR, "a[href*='ap_desktop_footer_privacy_notice']")
 # =============================== LOCATORS ================================================
 
 @then('Sign in page is opened')
@@ -27,5 +48,18 @@ def verify_on_sign_in(context):
     """
     Searches for unique elements of Sign In page to verify it is opened
     """
-    context.driver.find_element(*EMAIL_FIELD)
-    context.driver.find_element(*PASSWORD_FIELD)
+    # mandatory
+    # context.driver.find_element(*EMAIL_FIELD).
+    context.driver.find_element(*EMAIL_FIELD).send_keys("checking this field")
+    # context.driver.find_element(*PASSWORD_FIELD)
+    context.driver.find_element(*PASSWORD_FIELD).send_keys("checking PW field")
+    # in case
+    context.driver.find_element(*AMAZON_LOGO)
+    context.driver.find_element(*SIGN_IN_LOGO)
+    context.driver.find_element(*FORGOT_PASSWORD_LINK)
+    context.driver.find_element(*SIGN_IN_BTN)
+    context.driver.find_element(*KEEP_SIGNED_CHECKBOX)
+    context.driver.find_element(*DETAILS_LINK)
+    context.driver.find_element(*CREAT_ACCOUNT_LINK)
+    context.driver.find_element(*COU_LINK)
+    context.driver.find_element(*PRIVACY_NOTICE_LINK)
